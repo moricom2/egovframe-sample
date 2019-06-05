c76e5f0a656342678b97fd8bee2ee678a3528867
Chung sun mi <moricom@injeinc.co.kr>
2019-06-05 18:14:28 +0900

    egovframe 샘플코드 push

---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
+<jsp:forward page="/egovSampleList.do"/>
+<%--
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>test web</title>
+</head>
+<body>
+<img src="cone_sample2.png" width="100%" />
+</body>
+</html>
+--%>
---
+++ src/test/java/egovframework/example/JUnit4Test.java
@@ -0,0 +1,22 @@
+package egovframework.example;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class JUnit4Test {
+	
+	@Test
+	public void test() {
+		assertEquals(1, 1);
+	}
+	@Test
+	public void test2() {
+		assertEquals(1, 1);
+	}
+//	@Test
+//	public void test3() {
+//		assertEquals(1, 2);
+//	}
+
+}
